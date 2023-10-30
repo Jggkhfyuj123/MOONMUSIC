c58c2ffa5641335323266985bb24ddf4c3b86fd2
DIAMOND <101085046+MOON-CODES@users.noreply.github.com>
2023-10-30 19:53:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Ind-music/Vip",
+    "https://github.com/MOON-CODES/MOONMUSIC",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
